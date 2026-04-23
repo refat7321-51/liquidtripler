025a67300bbfe6ba39607f820e2d256bb3824df6
Md Jawwad Bin Kaysar Refat <rifat732151@gmail.com>
2026-04-23 17:04:05 +0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 # build_files.sh
 echo "Building the project..."
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput --clear
 echo "Build complete."
