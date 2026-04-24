a971afe21a096994e0096cc2123a102bc0f533f9
refat7321-51 <asmrifat908@gmail.com>
2026-04-25 01:47:40 +0600

    1

--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,5 @@
 echo "Building the project..."
 python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput --clear
+python3 manage.py migrate --noinput
 echo "Build complete."
